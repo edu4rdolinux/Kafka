599d2b5703021f8f8886658c0e00adf7e691f0de
edu4rdolinux <eduardosbarros.ed@gmail.com>
2024-12-07 19:02:11 -0300

    Nunito Sans Font

--- app.vue
+++ app.vue
@@ -1,13 +1,34 @@
 <template>
   <div>
-
+    <h1 class="nunito-sans">Hello, World!</h1>
   </div>
 </template>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,opsz,wght@0,6..12,200..1000;1,6..12,200..1000&family=Rubik:ital,wght@0,300..900;1,300..900&display=swap');
 
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  color: #f8fefb;
+  background-color: #020420;
+  font-family: "Nunito Sans", serif;
+}
+
+.nunito-sans {
+  font-family: "Nunito Sans", serif;
+  font-optical-sizing: auto;
+  font-weight: 400;
+  font-style: normal;
+  font-variation-settings:
+    "opsz" 8,
+    "wght" 400;
+}
 </style>
 
 <script lang="typescript">
-
 </script>
