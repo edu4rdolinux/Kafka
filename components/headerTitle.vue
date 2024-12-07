ef6c7ab5e06b55863a70f4bd1aafa5d04ff90592
edu4rdolinux <eduardosbarros.ed@gmail.com>
2024-12-07 19:22:56 -0300

    Title

---
+++ components/headerTitle.vue
@@ -0,0 +1,10 @@
+<template>
+    <h1 class="Title">Kafka</h1>
+</template>
+
+<style lang="scss">
+.Title {
+    position: fixed;
+    margin: 0.5em;
+}
+</style>
--- components/pageHeader.vue
+++ components/pageHeader.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="Header">
+        <HeaderTitle/>
     </div>
     <HeaderHr/>
 </template>
